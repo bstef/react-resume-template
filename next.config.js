a24e31ce84f0bb46921dba3543b964d1b10554ef
Ben Bobby Stef <benjaminstef@gmail.com>
2026-06-20 21:58:39 -0300

    fix next.config.js syntax and add unoptimized images

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
 /* eslint-env node */
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-    images: {
-    unoptimized: true,
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
-
-    // Next 12 has multiple TS loaders, and we need to update all of them.
     const tsRules = oneOfRule.oneOf.filter(rule => rule.test && rule.test.toString().includes('tsx|ts'));
-
     tsRules.forEach(rule => {
       // eslint-disable-next-line no-param-reassign
       rule.include = undefined;
     });
-
     return config;
   },
   compress: true,
@@ -27,11 +23,13 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: false,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },{
+      },
+      {
         protocol: 'https',
         hostname: 'source.unsplash.com',
       },
